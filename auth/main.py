28afa9606e280df554329e7ae212bdadc6a8d0f1
wilkdaniel03 <wilkdaniel03@gmail.com>
2025-12-28 00:53:26 +0100

    [api-auth] creating auth api

--- auth/main.py
+++ auth/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from auth!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/status")
+def get_status():
+    return { "status": 200 }
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app",port=8082)
