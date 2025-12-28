109ea9ad78d3d72022e753a0080f6032d04a1e3f
wilkdaniel03 <wilkdaniel03@gmail.com>
2025-12-28 01:07:16 +0100

    [api-auth] decoding secret into bytes

--- auth/main.py
+++ auth/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import uvicorn
+import base64
 
+secret = bytes(32)
 app = FastAPI()
 
 @app.get("/status")
@@ -8,4 +10,9 @@ def get_status():
     return { "status": 200 }
 
 if __name__ == "__main__":
+    with open("./secret","r") as file:
+        data = file.read(32)
+        secret = base64.b64decode(data)
+    print(secret)
+
     uvicorn.run("main:app",port=8082)
