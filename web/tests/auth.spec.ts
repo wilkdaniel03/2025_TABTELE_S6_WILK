e97005af97c7e02652e0ca1602aa5833be98c17a
wilkdaniel03 <wilkdaniel03@gmail.com>
2025-12-31 17:05:07 +0100

    [web-auth] testing auth routing

---
+++ web/tests/auth.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from 'playwright/test';
+
+const URL = "http://localhost:8080";
+
+test("Check links validity", async ({ page }) => {
+
+	await Promise.all([
+		await page.waitForTimeout(5000),
+		await page.goto(URL,{ waitUntil: "networkidle" }).then(() => {
+			expect(page.url() === `${URL}/auth/login`).toBeTruthy();
+		}),
+		await page.locator("button",{ hasText: "Create new account"}).click().then(() => {
+			expect(page.url() === `${URL}/auth/register`).toBeTruthy();
+		}),
+		await page.locator("a",{ hasText: "Already have an account" }).click().then(() => {
+			expect(page.url() === `${URL}/auth/login`).toBeTruthy();
+		}),
+		await page.locator("a",{ hasText: "Reset password" }).click().then(() => {
+			expect(page.url() === `${URL}/auth/forgot`).toBeTruthy();
+		}),
+		await page.locator("button",{ hasText: "Create new account" }).click().then(() => {
+			expect(page.url() === `${URL}/auth/register`).toBeTruthy();
+		}),
+		await page.waitForTimeout(1000)
+	]);
+});
