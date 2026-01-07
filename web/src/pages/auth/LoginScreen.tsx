03728fe98b6c69e960ce5abb816eb786d009abbf
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-08 00:57:05 +0100

    [web-auth] automatic route redirects based on token

--- web/src/pages/auth/LoginScreen.tsx
+++ web/src/pages/auth/LoginScreen.tsx
@@ -1,8 +1,8 @@
 import { Stack, Input, Button, Text, Field } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import AuthLayout from "../../components/auth/AuthLayout";
-import { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useMemo } from "react";
+import { useNavigate, Navigate } from 'react-router-dom';
 
 const URL = "http://bd.wilkdaniel.com:8081/token";
 
@@ -42,6 +42,12 @@ const Login = () => {
 	const [click, setClick] = useState<boolean>(false);
 	const [error, setError] = useState<string>("");
 	const navigate = useNavigate();
+	let should_redirect: boolean = false;
+
+	useMemo(() => {
+		if(localStorage.getItem("token"))
+			should_redirect = true;
+	},[]);
 
 	useEffect(() => {
 		if(!click) return;
@@ -54,45 +60,50 @@ const Login = () => {
 	},[click]);
 
     return (
-        <AuthLayout title="Welcome to fleet system" subtitle="Let's sign in">
-            <Stack>
-                <Field.Root>
-                    <Field.Label fontSize="14px" fontWeight="600" mb={2}>
-                        Username
-                    </Field.Label>
-                    <Input h="44px" placeholder="Your username" value={su} onChange={(e) => setSu(e.target.value)} />
-                </Field.Root>
+		<>
+			{!should_redirect
+			?
+			<AuthLayout title="Welcome to fleet system" subtitle="Let's sign in">
+				<Stack>
+					<Field.Root>
+						<Field.Label fontSize="14px" fontWeight="600" mb={2}>
+							Username
+						</Field.Label>
+						<Input h="44px" placeholder="Your username" value={su} onChange={(e) => setSu(e.target.value)} />
+					</Field.Root>
 
-                <Field.Root>
-                    <Field.Label fontSize="14px" fontWeight="600" mb={2}>
-                        Password
-                    </Field.Label>
-                    <Input h="44px" placeholder="Your password" type="password" value={sp} onChange={(e) => setSp(e.target.value)} />
-                </Field.Root>
+					<Field.Root>
+						<Field.Label fontSize="14px" fontWeight="600" mb={2}>
+							Password
+						</Field.Label>
+						<Input h="44px" placeholder="Your password" type="password" value={sp} onChange={(e) => setSp(e.target.value)} />
+					</Field.Root>
 
-				<span style={{color: "red"}}>{ error }</span>
+					<span style={{color: "red"}}>{ error }</span>
 
-                <Button
-                    h="48px"
-                    bg="#5B5BF7"
-                    color="white"
-                    _hover={{ bg: "#4B4BEA" }}
-                    onClick={() => setClick(true)}
-                >
-                    Sign in
-                </Button>
+					<Button
+						h="48px"
+						bg="#5B5BF7"
+						color="white"
+						_hover={{ bg: "#4B4BEA" }}
+						onClick={() => setClick(true)}
+					>
+						Sign in
+					</Button>
 
-                <Text fontSize="13px" color="gray.600" textAlign="center">
-                    <Link to="/auth/forgot">Reset password</Link>
-                </Text>
+					<Text fontSize="13px" color="gray.600" textAlign="center">
+						<Link to="/auth/forgot">Reset password</Link>
+					</Text>
 
-                <Button h="48px" bg="#0B84FF" color="white" _hover={{ bg: "#0A76E6" }}>
-                    <Link to="/auth/register" style={{ width: "100%", display: "block", textAlign: "center", color: "white" }}>
-                        Create new account
-                    </Link>
-                </Button>
-            </Stack>
-        </AuthLayout>
+					<Button h="48px" bg="#0B84FF" color="white" _hover={{ bg: "#0A76E6" }}>
+						<Link to="/auth/register" style={{ width: "100%", display: "block", textAlign: "center", color: "white" }}>
+							Create new account
+						</Link>
+					</Button>
+				</Stack>
+			</AuthLayout>
+			: <Navigate to="/dashboard/" replace={true}/>}
+		</>
     );
 }
 
