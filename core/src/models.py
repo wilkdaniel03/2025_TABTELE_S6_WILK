f1b81f3b882458d37b8506700ae6b8c42e6608d5
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-07 21:47:32 +0100

    [api-core] vehicle model

--- core/src/models.py
+++ core/src/models.py
@@ -23,3 +23,24 @@ class VehicleTypeDto:
     model: str
     version: str
     segment: str
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicle"
+
+    veh_id = Column(Integer,primary_key=True,autoincrement=True,nullable=False) 
+    vin = Column(String(50),nullable=False)
+    registration_number = Column(String(50),nullable=False)
+    current_mileage = Column(Integer,nullable=False)
+    production_year = Column(Integer,nullable=False)
+    status = Column(String,nullable=False)
+
+
+@dataclass
+class VehicleDto:
+    vin: str
+    registration_number: str
+    current_mileage: int
+    production_year: int
+    status: str
+    type: VehicleTypeDto
