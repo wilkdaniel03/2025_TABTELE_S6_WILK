bcd79585fd04ea413538a91de1666a7c18e16a44
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-09 18:08:37 +0100

    [web-dashboard] configuring import paths

--- web/webpack.common.js
+++ web/webpack.common.js
@@ -2,10 +2,16 @@ const path = require('path');
 const process = require('process');
 
 const MODE = process.env['MODE'] === 'dev' ? 'development' : 'production';
+const abs_path = process.cwd();
+
+const aliases = {};
+aliases["@features"] = path.join(abs_path,"src","features");
+aliases["@components"] = path.join(abs_path,"src","components");
+aliases["@pages"] = path.join(abs_path,"src","pages");
 
 module.exports = {
 	mode: MODE,
-	resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+	resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'], alias: aliases },
 	entry: path.join(__dirname,'src','main.tsx'),
 	output: {
 		publicPath: '/'
