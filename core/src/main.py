4c58f83f6fb5a3852d3d02a94298528b5990f999
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-10 19:50:31 +0100

    [api-core] get vehicle type route

--- core/src/main.py
+++ core/src/main.py
@@ -1,9 +1,11 @@
 import uvicorn
 from fastapi import FastAPI, Header, HTTPException
 from typing import Annotated
+from sqlalchemy import insert, select
 import connection
 import models
 import requests
+import loader
 
 
 app =  FastAPI()
@@ -33,6 +35,25 @@ def get_check(authorization: Annotated[str | None, Header()]):
     return res.json()
 
 
+@app.get("/vehicletype")
+def get_vehicletype():
+    with connection.ENGINE.connect() as conn:
+        sel = select(models.VehicleType)
+        res = conn.execute(sel).fetchall()
+        if len(res) == 0:
+            raise HTTPException(404,"Failed to find any vehicle types")
+        res = [models.VehicleTypeRec(*x) for x in res]
+        return res
+
+
+def load_data() -> None:
+    vehicle_type_data = loader.load_csv("vehicletype.csv")
+    with connection.ENGINE.connect() as conn:
+        conn.execute(insert(models.VehicleType).values(vehicle_type_data))
+        conn.commit()
+
+
 if __name__ == "__main__":
     models.init_db(connection.ENGINE)
+    load_data()
     uvicorn.run("main:app",port=8080,host="0.0.0.0")
