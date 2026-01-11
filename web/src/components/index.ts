a61c23ca9296f27d2890e78196159a6760459bee
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-11 17:28:16 +0100

    [web-dashboard] table component

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,7 +1,9 @@
 import LoadingButton from "./LoadingButton";
 import { LoadingButtonState } from "./LoadingButton";
+import Table from "./Table";
 
 export {
 	LoadingButton,
-	LoadingButtonState
+	LoadingButtonState,
+	Table
 }
