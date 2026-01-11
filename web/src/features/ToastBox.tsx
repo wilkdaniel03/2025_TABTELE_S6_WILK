cc8a3237ee48ab7b4cd0424eee5d2fc6c1e01a6e
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-11 21:31:50 +0100

    [web-dashboard] toast icon

--- web/src/features/ToastBox.tsx
+++ web/src/features/ToastBox.tsx
@@ -1,5 +1,46 @@
+import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 import * as Chakra from "@chakra-ui/react";
+import { MdErrorOutline } from "react-icons/md";
+import { BiError } from "react-icons/bi";
+import { MdDone } from "react-icons/md";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+
+enum ToastIconType {
+	ERROR = 0,
+	WARNING = 1,
+	SUCCESS = 2,
+	INFORMATION = 3,
+}
+
+interface IToastIconProps {
+	type: ToastIconType;
+}
+
+const ToastIcon = (props: IToastIconProps) => {
+	switch(props.type) {
+		case ToastIconType.ERROR: return <MdErrorOutline style={{color:"#ef4444"}}/>;
+		case ToastIconType.WARNING: return <BiError style={{color:"#eab308"}}/>;
+		case ToastIconType.SUCCESS: return <MdDone style={{color:"#22c55e"}}/>;
+		case ToastIconType.INFORMATION: return <IoMdInformationCircleOutline style={{color:"#3b82f6"}}/>;
+	}
+}
+
+interface IToastProps {
+	type: ToastIconType;
+	children: ReactNode;
+}
+
+const Toast = (props: IToastProps) => {
+	return (
+		<Chakra.Card.Root mb="10px" mr="10px">
+			<Chakra.Card.Body>
+				<ToastIcon type={props.type}/>
+				{props.children}
+			</Chakra.Card.Body>
+		</Chakra.Card.Root>
+	);
+}
 
 const ToastBox = () => {
 	const container = document.querySelector("#toastbox");
@@ -8,7 +49,7 @@ const ToastBox = () => {
 		return (
 			<>
 				{createPortal(
-					<Chakra.Heading>ToastBox</Chakra.Heading>,
+					<><Toast type={0}>a</Toast><Toast type={1}>b</Toast><Toast type={2}>c</Toast><Toast type={3}>d</Toast></>,
 					container
 				)}
 			</>
