9bd2da17f8ef537702cf7d1309be1eca5e75adf8
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-15 20:11:23 +0100

    [web-dashboard] avatar component onclick

--- web/src/components/Avatar.tsx
+++ web/src/components/Avatar.tsx
@@ -1,10 +1,14 @@
 import * as Chakra from "@chakra-ui/react";
 import AvatarImg from "@assets/avatar.png";
 
-const Avatar = () => {
+interface IAvatarProps {
+	onClick?: () => void;
+}
+
+const Avatar = (props: IAvatarProps) => {
 	return (
-		<Chakra.Box>
-			<img width="48px" src={AvatarImg}/>
+		<Chakra.Box width="100%" display="flex" justifyContent="end">
+			<img onClick={props.onClick} style={{cursor:"pointer"}} width="48px" src={AvatarImg}/>
 		</Chakra.Box>
 	);
 }
