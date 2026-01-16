7d3e26a63134500717e1a6d34faa0f8a0564850a
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-16 12:18:54 +0100

    [web-dashboard] append missing link in forgot page

--- web/src/pages/auth/ForgotPass.tsx
+++ web/src/pages/auth/ForgotPass.tsx
@@ -1,6 +1,8 @@
 import { Stack } from "@chakra-ui/react";
 import { AuthLayout } from "@components/auth";
 import { Form, IFormField } from "@components";
+import * as Chakra from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const FORM_FIELDS: IFormField[] = [
 	{ name: "email", label: "Email", placeholder: "Your email", type: "text"},
@@ -12,6 +14,11 @@ export default function ForgotPassword() {
         <AuthLayout title="Welcome to fleet system" subtitle="Reset password">
             <Stack>
 				<Form fields={FORM_FIELDS} submitLabel="Reset password" onSubmit={(data) => alert("Reset password")} />
+				<Chakra.Button bg="#0B84FF" fontWeight="semibold" color="white" _hover={{ bg: "#0A76E6" }}>
+					<Link to="/auth/register" style={{ width: "100%", display: "block", textAlign: "center", color: "white" }}>
+						Create new account
+					</Link>
+				</Chakra.Button>
             </Stack>
         </AuthLayout>
     );
