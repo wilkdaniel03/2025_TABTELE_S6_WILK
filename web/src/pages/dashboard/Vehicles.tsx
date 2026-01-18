e667fd9bcb323d457aec30e6ad31173766d1cebe
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-18 18:51:22 +0100

    [web-dashboard] delete handler finding checked items

--- web/src/pages/dashboard/Vehicles.tsx
+++ web/src/pages/dashboard/Vehicles.tsx
@@ -1,12 +1,23 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import * as Chakra from "@chakra-ui/react";
 import { VehicleTable } from "@features";
+import { useVehicleStore } from "@stores";
 
 const VehiclesPage = () => {
+	const { vehicles } = useVehicleStore();
+
+	const handleDelete = () => {
+		const ids = vehicles.filter((item) => item.checked).map((item) => item.id);
+		console.log(ids);
+	}
+
     return (
-        <Box p={6}>
-            <Heading mb={4}>Vehicles</Heading>
-			<Box w="50%"><VehicleTable/></Box>
-        </Box>
+        <Chakra.Box p={6}>
+            <Chakra.Heading mb={4}>Vehicles</Chakra.Heading>
+			<Chakra.Button bg="red.500" color="white" _hover={{ bg: "#0A76E6" }} fontWeight="semibold" onClick={handleDelete}>
+				Delete
+			</Chakra.Button>
+			<Chakra.Box w="50%"><VehicleTable/></Chakra.Box>
+        </Chakra.Box>
     );
 }
 
