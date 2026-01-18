cd1079f790bd127307899101109e039d01223d92
wilkdaniel03 <wilkdaniel03@gmail.com>
2026-01-18 17:07:55 +0100

    [web-dashboard] vehicle store usage in vehicle table

--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -1,10 +1,14 @@
 import { ToastIconType, useToastStore } from "./ToastStore";
 import { useReservationStore, reservationToString, reservationExtractChecked } from "./ReservationStore";
+import { useVehicleStore, vehicleToString, vehicleExtractChecked } from "./VehicleStore";
 
 export {
 	ToastIconType,
 	useToastStore,
 	useReservationStore,
 	reservationToString,
-	reservationExtractChecked
+	reservationExtractChecked,
+	useVehicleStore,
+	vehicleToString,
+	vehicleExtractChecked
 }
